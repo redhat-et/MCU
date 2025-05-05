d52b06bbea118d55095eaefa0f46c5aa58dee2a8
Alessandro Sangiorgi <asangior@redhat.com>
2025-05-05 15:56:14 -0500

    Add last modified time on Kernel

--- tcm/triton_cache_manager/models/kernel.py
+++ tcm/triton_cache_manager/models/kernel.py
@@ -6,7 +6,12 @@ This module defines the data structures used to represent kernels and their file
 
 from dataclasses import dataclass
 from pathlib import Path
+import datetime
+import logging
 from typing import Mapping, Any, Optional
+
+
+log = logging.getLogger(__name__)
 
 
 @dataclass(slots=True)
@@ -66,3 +71,15 @@ class Kernel:
     kpack: Optional[int]
     matrix_instr_nonkdim: Optional[int]
     files: list[KernelFile]
+    modified_time: Optional[float] = None
+
+    @property
+    def modified_datetime(self) -> Optional[datetime.datetime]:
+        """Return modification time as a datetime object."""
+        if self.modified_time is None:
+            return None
+        try:
+            return datetime.datetime.fromtimestamp(self.modified_time)
+        except (ValueError, TypeError):
+            log.debug("Value or Type error for modified_time")
+            return None
