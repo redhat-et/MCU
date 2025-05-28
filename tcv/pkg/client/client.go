4a5f8bd30a311057140e431cf2b7a5add98c1d9f
Maryam Tahhan <mtahhan@redhat.com>
2025-05-28 09:41:23 -0400

    chore: add client api to get GPU info
    Signed-off-by: Maryam Tahhan <mtahhan@redhat.com>

--- tcv/pkg/client/client.go
+++ tcv/pkg/client/client.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/redhat-et/TKDK/tcv/pkg/accelerator"
+	"github.com/redhat-et/TKDK/tcv/pkg/accelerator/devices"
 	"github.com/redhat-et/TKDK/tcv/pkg/config"
 	"github.com/redhat-et/TKDK/tcv/pkg/constants"
 	"github.com/redhat-et/TKDK/tcv/pkg/fetcher"
 	"github.com/redhat-et/TKDK/tcv/pkg/logformat"
+	"github.com/redhat-et/TKDK/tcv/pkg/preflightcheck"
 	logging "github.com/sirupsen/logrus"
 )
 
@@ -61,15 +64,32 @@ func ExtractCache(opts Options) error {
 	return fetcher.New().FetchAndExtractCache(opts.ImageName)
 }
 
-func loggingLevelFromString(level string) error {
-	if level == "" {
-		level = "info"
+// GetSystemGPUInfo initializes the GPU accelerator and returns the list of available GPU devices
+func GetSystemGPUInfo() ([]devices.TritonGPUInfo, error) {
+	if _, err := config.Initialize(config.ConfDir); err != nil {
+		return nil, fmt.Errorf("failed to initialize config: %w", err)
 	}
-	lvl, err := logging.ParseLevel(level)
+
+	// Assume GPU is enabled if we're requesting GPU info
+	if !config.IsGPUEnabled() {
+		logging.Debug("Forcing ENABLE_GPU=true to retrieve GPU info")
+		config.SetEnabledGPU(true)
+	}
+
+	// Initialize the GPU accelerator
+	acc, err := accelerator.New(config.GPU, true)
 	if err != nil {
-		return err
+		return nil, fmt.Errorf("failed to initialize GPU accelerator: %w", err)
 	}
-	logging.SetLevel(lvl)
-	logging.SetReportCaller(true)
-	return nil
+
+	// Register the accelerator
+	accelerator.GetRegistry().MustRegister(acc)
+
+	// Fetch GPU device information
+	info, err := preflightcheck.GetAllGPUInfo(acc)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get GPU info: %w", err)
+	}
+
+	return info, nil
 }
