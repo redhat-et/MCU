c3bc7653c9f758aa63b8234567b161c893935f7a
UmerHA <40663591+UmerHA@users.noreply.github.com>
2025-05-29 11:43:11 +0200

    Contributing triton_util

---
+++ triton_util/triton_util/coding.py
@@ -0,0 +1,89 @@
+import triton
+import triton.language as tl
+from triton.language import constexpr as const
+
+def cdiv(a,b): return (a + b - 1) // b
+
+# # offsets
+
+@triton.jit
+def offset_1d(sz: const, n_prev_chunks=0): return n_prev_chunks * sz + tl.arange(0, sz)
+
+@triton.jit
+def offset_2d(offs0, offs1, stride0, stride1=1):  return tl.expand_dims(offs0, 1)*stride0 + tl.expand_dims(offs1, 0)*stride1
+
+# # masks
+
+@triton.jit
+def mask_1d(offs, max): return offs < max
+
+@triton.jit
+def mask_2d(offs0, offs1, max0, max1): return (tl.expand_dims(offs0, 1) < max0) & (tl.expand_dims(offs1, 0) < max1)
+
+# # load
+
+@triton.jit
+def load_1d(ptr, sz: const, n, max, stride=1):
+    '''Chunk 1d vector (defined by ptr) into 1d grid, where each chunk has size sz. Load the nth chunk. Ie, load [n*sz,...,(n+1)*sz-1].'''
+    offs = offset_1d(sz, n)
+    mask = mask_1d(offs, max)
+    return tl.load(ptr + offs, mask) 
+
+@triton.jit
+def load_full_1d(ptr, sz: const, stride=1):
+    '''Load 1d block [0,...,sz-1]'''
+    offs = offset_1d(sz)
+    mask = mask_1d(offs, sz)
+    return tl.load(ptr + offs, mask) 
+
+@triton.jit
+def load_2d(ptr, sz0: const, sz1: const, n0, n1, max0, max1, stride0=None, stride1=1):
+    '''Chunk 2d matrix (defined by ptr) into 2d grid, where each chunk has size (sz0,sz1). Load the (n0,n1)th chunk. Ie, load [n0*sz0,...,(n0+1)*sz0-1] x [n1*sz1,...,(n1+1)*sz1-1].'''
+    stride0 = stride0 or sz1
+    offs0 = offset_1d(sz0, n0)
+    offs1 = offset_1d(sz1, n1)        
+    offs = offset_2d(offs0, offs1, stride0, stride1)
+    mask = mask_2d(offs0, offs1, max0, max1)
+    return tl.load(ptr + offs, mask) 
+
+@triton.jit
+def load_full_2d(ptr, sz0: const, sz1: const, stride0=None, stride1=1):
+    '''Load 2d block [0,...,sz0-1] x [0,...,sz1-1] '''
+    stride0 = stride0 or sz1
+    offs = offset_2d(tl.arange(0, sz0), tl.arange(0, sz1), stride0, stride1)
+    mask = mask_2d(  tl.arange(0, sz0), tl.arange(0, sz1), sz0, sz1)
+    return tl.load(ptr + offs, mask) 
+
+# # store
+
+@triton.jit
+def store_1d(vals, ptr, sz: const, n, max, stride=1):
+    '''Store 1d block into nth chunk of vector (defined by ptr), where each chunk has size sz'''
+    offs = offset_1d(sz, n)
+    mask = mask_1d(offs, max)
+    tl.store(ptr + offs, vals, mask)
+
+@triton.jit
+def store_full_1d(vals, ptr, sz: const, stride=1):
+    '''Store 1d block into vector (defined by ptr)'''
+    offs = offset_1d(sz)
+    mask = mask_1d(offs, sz)
+    tl.store(ptr + offs, vals, mask)
+
+@triton.jit
+def store_2d(vals, ptr, sz0: const, sz1: const, n0, n1, max0, max1, stride0=None, stride1=1):
+    '''Store 2d block into (n0,n1)th chunk of matrix (defined by ptr), where each chunk has size (sz0, sz1)'''
+    stride0 = stride0 or sz1
+    offs0 = offset_1d(sz0, n0)
+    offs1 = offset_1d(sz1, n1)        
+    offs = offset_2d(offs0, offs1, stride0, stride1)
+    mask = mask_2d(offs0, offs1, max0, max1)
+    tl.store(ptr + offs, vals, mask)
+
+@triton.jit
+def store_full_2d(vals, ptr, sz0: const, sz1: const, stride0=None, stride1=1):
+    '''Store 2d block into matrix (defined by ptr)'''
+    stride0 = stride0 or sz1
+    offs = offset_2d(tl.arange(0, sz0), tl.arange(0, sz1), stride0, stride1)
+    mask = mask_2d(  tl.arange(0, sz0), tl.arange(0, sz1), sz0, sz1)
+    tl.store(ptr + offs, vals, mask)
---
+++ triton_util/triton_util/debugging.py
@@ -0,0 +1,58 @@
+import os
+import triton
+import triton.language as tl
+
+def _test_pid_conds(conds, pid0=0, pid1=0, pid2=0):
+    '''Test if condition on pids are fulfilled
+    E.g.:
+        '=0'    checks that pid_0 == 0
+        ',>1'   checks that pid_1 > 1
+        '>1,=0' checks that pid_0 > 1 and pid_1 == 0
+    '''
+    pids = pid0, pid1, pid2
+    conds = conds.replace(' ','').split(',')
+    for i, (cond, pid) in enumerate(zip(conds, pids)):
+        if cond=='': continue
+        if   cond[:2] in ['<=', '>=', '!=']: op, threshold = cond[:2], int(cond[2:])
+        elif cond[:1] in ['<',  '>',  '=' ]: op, threshold = cond[:1], int(cond[1:])        
+        else: raise ValueError(f"Rules may only use these ops: '<','>','>=','<=','=', '!='. Invalid rule in '{cond}'.")
+        op = '==' if op == '=' else op
+        if not eval(f'{pid} {op} {threshold}'): return False
+    return True
+
+@triton.jit
+def test_pid_conds(conds):
+    '''Test if condition on pids are fulfilled
+    E.g.:
+        '=0'    checks that pid_0 == 0
+        ',>1'   checks that pid_1 > 1
+        '>1,=0' checks that pid_0 > 1 and pid_1 == 0
+    '''
+    return _test_pid_conds(conds, tl.program_id(0).handle.data[0], tl.program_id(1).handle.data[0], tl.program_id(2).handle.data[0])
+
+@triton.jit
+def breakpoint_if(conds):
+    '''Stop kernel, if condition on pids is fulfilled'''
+    from IPython.core.debugger import set_trace
+    if test_pid_conds(conds): set_trace()
+
+@triton.jit
+def print_if(*txt, conds):
+    '''Print txt, if condition on pids is fulfilled'''
+    if test_pid_conds(conds): print(*txt)
+
+@triton.jit
+def breakpoint_once(): breakpoint_if('=0,=0,=0')
+
+@triton.jit
+def print_once(*txt): print_if(*txt,conds='=0,=0,=0')
+
+def assert_tensors_gpu_ready(*tensors):
+    for t in tensors:
+        assert t.is_contiguous(), "A tensor is not contiguous"
+        if not os.environ.get('TRITON_INTERPRET') == '1': assert t.is_cuda, "A tensor is not on cuda"
+
+@triton.jit
+def offsets_from_base(ptrs, base_ptr):
+    '''Return offsets for which ptrs = base_ptr + offsets''' # todo umer: write test
+    return ptrs.to(tl.uint64) - base_ptr.to(tl.uint64)
