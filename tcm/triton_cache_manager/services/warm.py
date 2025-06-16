b5508e139f873d72b0f73bfb59d2487230ce1040
Alessandro Sangiorgi <asangior@redhat.com>
2025-06-16 15:53:05 -0500

    Add rocm backend Add VLLM cache root as argument Add date and time dir for the warmed cache saved in the host Several other minor changes

--- tcm/triton_cache_manager/services/warm.py
+++ tcm/triton_cache_manager/services/warm.py
@@ -5,6 +5,8 @@ Service for warming the vLLM cache using a container.
 import logging
 import subprocess
 import shutil
+import os
+from datetime import datetime
 from pathlib import Path
 
 from ..utils.paths import get_cache_dir
@@ -33,7 +35,7 @@ class WarmupService:
         self.host_cache_dir = host_cache_dir
         self.model = model
 
-    def warmup(self, image: str, output_file: Path) -> bool:
+    def warmup(self, image: str, output_file: Path, tarball: bool, rocm: bool) -> bool:
         """
         Executes the full cache warming and packaging workflow.
 
@@ -49,26 +51,43 @@ class WarmupService:
         """
         log.info("Starting cache warmup process with image: '%s'", image)
 
-        #if not self._run_container(image):
+        # if not self._run_container(image):
         #    log.error("Cache warming container failed to execute successfully.")
         #    return False
-        self._run_container(image)
+        self._run_container(image, rocm)
 
-        log.info("Container completed successfully. Now packaging the cache...")
+        if tarball:
+            log.info("Container completed successfully. Now packaging the cache...")
+            try:
+                self._package_cache(output_file)
+                final_size = output_file.stat().st_size
+                log.info(
+                    "Cache packaged successfully to '%s' (Size: %s)",
+                    output_file,
+                    format_size(final_size),
+                )
+                return True
+            except (OSError, shutil.Error) as e:
+                log.error("Failed to package the cache directory: %s", e, exc_info=True)
+                return False
+        return True
+
+    def _create_date_time_dir(self) -> str:
+        """Create a date time directory for the warmed cache"""
+        now = datetime.now()
+        timestamp = now.strftime("%Y-%m-%d_%H-%M-%S")
+        dir_name = f"{self.host_cache_dir}/cache_{timestamp}"
         try:
-            self._package_cache(output_file)
-            final_size = output_file.stat().st_size
-            log.info(
-                "Cache packaged successfully to '%s' (Size: %s)",
-                output_file,
-                format_size(final_size),
-            )
-            return True
-        except (OSError, shutil.Error) as e:
-            log.error("Failed to package the cache directory: %s", e, exc_info=True)
-            return False
+            os.makedirs(dir_name)
+            print(f"Directory '{dir_name}' created successfully.")
+        except FileExistsError:
+            log.error("Directory %s already exists.", dir_name)
+        except Exception as e:
+            log.error("Error creating directory: %s", e)
 
-    def _run_container(self, image: str) -> bool:
+        return dir_name
+
+    def _run_container(self, image: str, rocm: bool) -> bool:
         """
         Runs a Podman container with the cache directory mounted as a volume.
 
@@ -78,28 +97,59 @@ class WarmupService:
         Returns:
             True if the container exits with a status code of 0, False otherwise.
         """
-        volume_mount = f"{self.host_cache_dir}:/root/.cache/vllm/:Z,rw"
+
+        env = {
+            "MODEL_NAME": self.model,
+            "VLLM_CACHE_ROOT": self.vllm_cache_dir,
+        }
+        if self.hface_secret:
+            env["HF_TOKEN"] = self.hface_secret
+
+        env_flags = [
+            part for key, val in env.items() for part in ("-e", f"{key}={val}")
+        ]
+
+        host_cache_dir = self._create_date_time_dir()
+        if rocm:
+            backend_cmd = [
+                "--ipc=host",
+                "--network=host",
+                "--cap-add=CAP_SYS_ADMIN",
+                "--device=/dev/kfd",
+                "--device=/dev/dri",
+                "--device=/dev/mem",
+                "--group-add",
+                "render",
+                "--cap-add=SYS_PTRACE",
+                "--security-opt",
+                "seccomp=unconfined",
+            ]
+        else:
+            backend_cmd = [
+                "--device",
+                "nvidia.com/gpu=all",
+            ]
+
+        volume_mount = f"{host_cache_dir}:{self.vllm_cache_dir}:Z,rw"
         command = [
             "podman",
             "run",
             "--privileged",
-            "-d",
             "-it",
-            "--device",
-            "nvidia.com/gpu=all",
+            *backend_cmd,
+            *env_flags,
             "--volume",
             volume_mount,
             image,
         ]
-        print(f"{command}")
 
         log.debug("Executing Podman command: %s", " ".join(command))
         try:
             result = subprocess.run(
                 command, check=True, capture_output=False, text=True, encoding="utf-8"
             )
-            #log.debug("Container stdout:\n%s", result.stdout)
-            #log.debug("Container stderr:\n%s", result.stderr)
+            log.debug("Container stdout:\n%s", result.stdout)
+            log.debug("Container stderr:\n%s", result.stderr)
             log.info("Podman container ran and exited successfully.")
             return True
         except FileNotFoundError:
@@ -113,7 +163,7 @@ class WarmupService:
                 "Podman container exited with a non-zero status code: %d.", e.returncode
             )
             log.error("Container stdout:\n%s", e.stdout)
-            #log.error("Container stderr:\n%s", e.stderr)
+            log.error("Container stderr:\n%s", e.stderr)
             return False
 
     def _package_cache(self, output_file: Path):
