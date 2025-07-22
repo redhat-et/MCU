d65bdc2ec3e5e837918f96ea1735ec53b91c9c3a
Alessandro Sangiorgi <asangior@redhat.com>
2025-07-22 13:38:40 -0500

    Minor fixes

--- tcm/triton_cache_manager/models/criteria.py
+++ tcm/triton_cache_manager/models/criteria.py
@@ -6,7 +6,7 @@ from dataclasses import dataclass
 from pathlib import Path
 from typing import Optional
 
-
+# pylint: disable=too-many-instance-attributes
 @dataclass
 class SearchCriteria:
     """Data structure to hold kernel search filter criteria."""
