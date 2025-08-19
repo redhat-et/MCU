4f7d31ac24081c1c8509e2b6b5e80abf717625eb
Alessandro Sangiorgi <asangior@redhat.com>
2025-08-19 14:53:36 -0500

    Add mcm release workflow based on tag mcm-v Add modules on init and pyproject

--- mcm/model_cache_manager/__init__.py
+++ mcm/model_cache_manager/__init__.py
@@ -1,6 +1,6 @@
 """
 Triton Cache Manager - A tool for managing Triton kernel cache.
 """
+__all__ = ["cli", "services", "data", "plugins", "utils", "models", "strategies", "runtime"]
 
-__all__ = ["cli", "services", "data", "plugins", "utils", "models"]
 __version__ = "0.1.0"
