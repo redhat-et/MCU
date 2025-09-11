bc85ab4c336224ee8c0ffe9ee78802b8cf61fd83
Maryam Tahhan <mtahhan@redhat.com>
2025-09-11 14:37:03 +0100

    mcv: add builder option
    Signed-off-by: Maryam Tahhan <mtahhan@redhat.com>

--- mcv/pkg/imgbuild/builder_test.go
+++ mcv/pkg/imgbuild/builder_test.go
@@ -11,7 +11,7 @@ func TestNew_BuildahAvailable(t *testing.T) {
 	defer func() { HasApp = origHasApp }()
 
 	HasApp = func(tool string) bool {
-		return tool == "buildah"
+		return tool == Buildah
 	}
 
 	builder, err := New()
@@ -24,7 +24,7 @@ func TestNew_DockerFallback(t *testing.T) {
 	defer func() { HasApp = origHasApp }()
 
 	HasApp = func(tool string) bool {
-		return tool == "docker"
+		return tool == Docker
 	}
 
 	builder, err := New()
@@ -43,4 +43,49 @@ func TestNew_Unsupported(t *testing.T) {
 	builder, err := New()
 	assert.Nil(t, builder)
 	assert.Error(t, err)
+}
+
+func TestNewWithBuilder_Buildah(t *testing.T) {
+	origHasApp := HasApp
+	defer func() { HasApp = origHasApp }()
+
+	HasApp = func(tool string) bool {
+		return tool == Buildah
+	}
+
+	builder, err := NewWithBuilder(Buildah)
+	assert.NoError(t, err)
+	assert.IsType(t, &buildahBuilder{}, builder)
+}
+
+func TestNewWithBuilder_Docker(t *testing.T) {
+	origHasApp := HasApp
+	defer func() { HasApp = origHasApp }()
+
+	HasApp = func(tool string) bool {
+		return tool == Docker
+	}
+
+	builder, err := NewWithBuilder(Docker)
+	assert.NoError(t, err)
+	assert.IsType(t, &dockerBuilder{}, builder)
+}
+
+func TestNewWithBuilder_Unsupported(t *testing.T) {
+	builder, err := NewWithBuilder("unsupported")
+	assert.Nil(t, builder)
+	assert.Error(t, err)
+}
+
+func TestNewWithBuilder_AutoDetect(t *testing.T) {
+	origHasApp := HasApp
+	defer func() { HasApp = origHasApp }()
+
+	HasApp = func(tool string) bool {
+		return tool == Docker
+	}
+
+	builder, err := NewWithBuilder("")
+	assert.NoError(t, err)
+	assert.IsType(t, &dockerBuilder{}, builder)
 }
