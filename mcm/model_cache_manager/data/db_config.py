c52f15674cd23451ab611e7aae6c71af4301cd2d
Alessandro Sangiorgi <asangior@redhat.com>
2025-10-15 08:54:33 -0500

    pylint

--- mcm/model_cache_manager/data/db_config.py
+++ mcm/model_cache_manager/data/db_config.py
@@ -19,6 +19,7 @@ engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
+# pylint: disable=invalid-name
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
